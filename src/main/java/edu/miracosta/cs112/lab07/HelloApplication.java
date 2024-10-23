2e3f832cba8338762613354a9ed367ed40e4d732
Richard Nugent <rickyn0915@gmail.com>
2024-10-23 14:45:04 -0700

    Lab Complete

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -1,6 +1,7 @@
 package edu.miracosta.cs112.lab07;//package name here depending on your IDE
 
 import javafx.application.Application;  //abstract class used for JavaFX GUI's
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;              //class for GUI window
 import javafx.scene.Scene;              //class for specific view in GUI window
@@ -10,7 +11,7 @@ import javafx.scene.control.Button;     //class for button component
 import javafx.event.EventHandler;       //interface for handling events
 import javafx.event.ActionEvent;        //class for type of event for action (like button or key pressed)
 
-public class HelloApplication extends Application  { //inheriting core functionality + this class will handle events
+public class HelloApplication extends Application implements EventHandler<ActionEvent> { //inheriting core functionality + this class will handle events
     /*** GUI COMPONENTS ***/
     // TODO: follow step 25 in README.md to create reference variables
 
@@ -19,7 +20,10 @@ public class HelloApplication extends Application  { //inheriting core functiona
         launch(args); //method from Application class, must be called to setup javafx application
     }
 
-    public void start(Stage primaryStage) {
+    Button button;
+
+    Label label;
+    public void start(Stage primaryStage)  {
         primaryStage.setTitle("Hello GUI: Richard Nugent");
 
         StackPane layout = new StackPane();
@@ -28,9 +32,26 @@ public class HelloApplication extends Application  { //inheriting core functiona
         primaryStage.setScene(scene);
                 primaryStage.show();
 
-                Label label = new Label( "Hello World!");
-                layout.getChildren().add(label);
+         label = new Label( "Hello World!");
+
+                AnchorPane anchorPane = new AnchorPane();
+                anchorPane.setBottomAnchor(label, 0.0);
+                anchorPane.setRightAnchor(label, 0.0);
+                anchorPane.getChildren().add(label);
+                layout.getChildren().add(anchorPane);
+
+                button = new Button( "Click Me!");
+                anchorPane.setBottomAnchor(button, 0.0);
+                anchorPane.setLeftAnchor(button, 0.0);
+                anchorPane.getChildren().add(button);
+                button.setOnAction(this);
     }
+    public void handle(ActionEvent e) {
+        if (e.getSource() == button) {
+            label.setText("New Text");
+        }
+    }
+
 
     // TODO: follow steps 2-9 in README.md to create a start method
 
